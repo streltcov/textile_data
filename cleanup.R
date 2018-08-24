e662e30768157573913c3e868e512bcc866d64c4
Петр Стрельцов <streltsov83@yandex.ru>
2018-08-24 03:49:39 +0300

    switched to python3

--- cleanup.R
+++
@@ -1,7 +0,0 @@
-# basic dataframe
-remove(textile_data)
-# densities - frame
-remove(densities)
-# densities - variables
-remove(dens_warp)
-remove(dens_weft)
--- densities/dens_describe.R
+++
@@ -1,3 +0,0 @@
-# Коэффициент соотношения плотностей
-# c(dens_warp / dens_weft)
-densities$ratio <- c(dens_warp / dens_weft)
