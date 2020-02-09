41eef0b40eb57fe91e1210088bbf173a17c73658
streltcov <streltsov83@yandex.ru>
2020-02-09 23:49:36 +0300

    modules.dataframes.py, densities and threads notebooks

---
+++ modules/dataframes.py
@@ -0,0 +1,50 @@
+import pandas
+
+
+def convert_to_float(value):
+    """
+    Преобразование значений в столбцах датафрейма;
+    :param value: float
+    :return: float
+    """
+    value = str(value)
+    value = value.replace(",", ".")
+    value = round(float(value), 2)
+    return value
+
+#######################################################################################################################
+
+
+
+# Загрузка датафрейма из CSV файла;
+textiles = pandas.read_csv('dataframe.csv')
+
+len_textiles_base = len(textiles)
+textiles = textiles.dropna(subset=['warp_dens', 'weft_dens'])
+len_textiles = len(textiles)
+
+########################################################################################################################
+
+# Приведение типов колонок датафрейма к значению с плавающей запятой;
+textiles['warp_a'] = textiles.warp_a.apply(convert_to_float)
+textiles['warp_b'] = textiles.warp_b.apply(convert_to_float)
+textiles['weft_a'] = textiles.weft_a.apply(convert_to_float)
+textiles['weft_b'] = textiles.weft_b.apply(convert_to_float)
+
+########################################################################################################################
+
+# Вычисление "усредненных" толщин нитей;
+textiles['warp_nominal'] = round((textiles['warp_a'] + textiles['warp_b']) / 2, 2)
+textiles['weft_nominal'] = round((textiles['weft_a'] + textiles['weft_b']) / 2, 2)
+
+########################################################################################################################
+
+# Соотношение плотностей по основе и утку;
+textiles['warp_weft'] = round((textiles['warp_dens'] / textiles['weft_dens']), 2)
+
+########################################################################################################################
+
+# Добавление признака density_coefficient;
+textiles['density_coefficient'] = round((textiles['warp_dens'] / textiles['weft_dens']), 2)
+
+########################################################################################################################
